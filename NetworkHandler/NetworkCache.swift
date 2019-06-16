978a2387d41acd33ed021214d3268ec064f4ff6e
Michael Redig <mredig@gmail.com>
2019-06-16 18:54:57 -0500

    added documentation to network cache

--- NetworkHandler/NetworkCache.swift
+++ NetworkHandler/NetworkCache.swift
@@ -8,9 +8,20 @@
 
 import Foundation
 
+/**
+Essentially just a wrapper for NSCache, but specifically purposed for use with
+NetworkHandler and does the work of converting URL -> NSURL and Data -> NSData
+for you. Directly exposes some properties like `countLimit` and `totalCostLimit`
+*/
 public class NetworkCache {
 	private let cache = NSCache<NSURL, NSData>()
 
+	/**
+	The maximum number of objects the cache should hold.
+
+	If 0, there is no count limit. The default value is 0.
+	This is not a strict limit—if the cache goes over the limit, an object in the cache could be evicted instantly, later, or possibly never, depending on the implementation details of the cache.
+	*/
 	public var countLimit: Int {
 		get {
 			return cache.countLimit
@@ -20,6 +31,13 @@ public class NetworkCache {
 		}
 	}
 
+	/**
+	The maximum total cost that the cache can hold before it starts evicting objects.
+
+	If 0, there is no total cost limit. The default value is 0.
+	When you add an object to the cache, you may pass in a specified cost for the object, such as the size in bytes of the object. If adding this object to the cache causes the cache’s total cost to rise above totalCostLimit, the cache may automatically evict objects until its total cost falls below totalCostLimit. The order in which the cache evicts objects is not guaranteed.
+	This is not a strict limit, and if the cache goes over the limit, an object in the cache could be evicted instantly, at a later point in time, or possibly never, all depending on the implementation details of the cache.
+	*/
 	public var totalCostLimit: Int {
 		get {
 			return cache.totalCostLimit
@@ -29,6 +47,7 @@ public class NetworkCache {
 		}
 	}
 
+	/// The name of the cache. The default is "NetworkHandler: NetworkCache"
 	public var name: String {
 		get {
 			return cache.name
