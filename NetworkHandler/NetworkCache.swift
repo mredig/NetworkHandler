9c0c4448d982c2102a3e06ad67edcba15b52d87d
Michael Redig <mredig@gmail.com>
2019-07-03 20:08:20 -0500

    code doc update

--- NetworkHandler/NetworkCache.swift
+++ NetworkHandler/NetworkCache.swift
@@ -34,8 +34,8 @@ public class NetworkCache {
 	/**
 	The maximum total cost that the cache can hold before it starts evicting objects.
 
-	If 0, there is no total cost limit. The default value is 0.
-	When you add an object to the cache, you may pass in a specified cost for the object, such as the size in bytes of the object. If adding this object to the cache causes the cache’s total cost to rise above totalCostLimit, the cache may automatically evict objects until its total cost falls below totalCostLimit. The order in which the cache evicts objects is not guaranteed.
+	If `0`, there is no total cost limit. The default value is `0`.
+	When you add an object to the cache, you may pass in a specified cost for the object, such as the size in bytes of the object. If adding this object to the cache causes the cache’s total cost to rise above `totalCostLimit`, the cache may automatically evict objects until its total cost falls below `totalCostLimit`. The order in which the cache evicts objects is not guaranteed.
 	This is not a strict limit, and if the cache goes over the limit, an object in the cache could be evicted instantly, at a later point in time, or possibly never, all depending on the implementation details of the cache.
 	*/
 	public var totalCostLimit: Int {
