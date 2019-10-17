769890c75ef9a559a44018f50d0708582f9eacc1
Michael Redig <mredig@gmail.com>
2019-10-16 20:39:55 -0500

    added macos target
    added macos tests
    juggled files to organize for multiple platforms
    added pngData extension for NSImage

---
+++ macOS/NetworkHandler_MacOS.h
@@ -0,0 +1,19 @@
+//
+//  NetworkHandler_MacOS.h
+//  NetworkHandler-MacOS
+//
+//  Created by Michael Redig on 10/16/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkHandler_MacOS.
+FOUNDATION_EXPORT double NetworkHandler_MacOSVersionNumber;
+
+//! Project version string for NetworkHandler_MacOS.
+FOUNDATION_EXPORT const unsigned char NetworkHandler_MacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkHandler_MacOS/PublicHeader.h>
+
+
