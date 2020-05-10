8d1e33da6e92701ce5a2e31252dd9cbfd984378a
Michael Redig <mredig@gmail.com>
2020-05-10 18:15:48 -0500

    (feat) added network request header tests

--- Tests/NetworkHandlerTests/NetworkRequestTests.swift
+++ Tests/NetworkHandlerTests/NetworkRequestTests.swift
@@ -27,4 +27,28 @@ class NetworkRequestTests: XCTestCase {
 		XCTAssertEqual(testDummy, try request.decoder.decode(DummyType.self, from: request.httpBody!))
 	}
 
+	/// Tests adding, setting, and getting header values
+	func testRequestHeaders() {
+		let dummyURL = URL(string: "https://redeggproductions.com")!
+		var request = dummyURL.request
+
+		request.addValue(.contentType(type: .json), forHTTPHeaderField: .commonKey(key: .contentType))
+		XCTAssertEqual("application/json", request.value(forHTTPHeaderField: .commonKey(key: .contentType)))
+		request.setValue(.contentType(type: .xml), forHTTPHeaderField: .commonKey(key: .contentType))
+		XCTAssertEqual("application/xml", request.value(forHTTPHeaderField: .commonKey(key: .contentType)))
+		request.setValue(.other(value: "Bearer: 12345"), forHTTPHeaderField: .commonKey(key: .authorization))
+		XCTAssertEqual(["Content-Type": "application/xml", "Authorization": "Bearer: 12345"], request.allHeaderFields)
+
+		request.setValue(nil, forHTTPHeaderField: .commonKey(key: .authorization))
+		XCTAssertEqual(["Content-Type": "application/xml"], request.allHeaderFields)
+		XCTAssertNil(request.value(forHTTPHeaderField: .commonKey(key: .authorization)))
+
+		request.setValue(.other(value: "Arbitrary Value"), forHTTPHeaderField: .other(key: "Arbitrary Key"))
+		XCTAssertEqual(["Content-Type": "application/xml", "Arbitrary Key": "Arbitrary Value"], request.allHeaderFields)
+
+		var request2 = dummyURL.request
+		request2.setValue(.contentType(type: .audioMp4), forHTTPHeaderField: .commonKey(key: .contentType))
+		XCTAssertEqual("audio/mp4", request2.value(forHTTPHeaderField: .commonKey(key: .contentType)))
+	}
+
 }
