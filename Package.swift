ac048c744286639726265a8742827a3361b59164
Michael Redig <mredig@gmail.com>
2020-12-01 16:57:07 -0600

    (feat) added cryptoswift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "NetworkHandler",
+	platforms: [
+		.macOS(.v10_12),
+		.iOS(.v9),
+		.tvOS(.v9),
+	],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -14,13 +19,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+		.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.7"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NetworkHandler",
-            dependencies: []),
+            dependencies: [
+				"CryptoSwift",
+			]),
         .testTarget(
             name: "NetworkHandlerTests",
             dependencies: ["NetworkHandler"]),
