c069c7196ef6b060728180f3ac977a2e9ddc192c
Michael Redig <mredig@gmail.com>
2021-03-18 21:27:48 -0500

    (feat) added multipart gen test

--- Tests/NetworkHandlerTests/MultipartInputStreamTests.swift
+++ Tests/NetworkHandlerTests/MultipartInputStreamTests.swift
@@ -83,6 +83,48 @@ class MultipartInputStreamTests: NetworkHandlerBaseTest {
 		XCTAssertEqual(expectedFinal, finalString)
 	}
 
+	func testMultipartGeneration() throws {
+		let boundary = "alskdglkasdjfglkajsdf"
+		let multipart = MultipartFormInputStream(boundary: boundary)
+
+		let arbText = "Odd input stream"
+		let arbitraryData = arbText.data(using: .utf8)!
+		let arbitraryStream = InputStream(data: arbitraryData)
+
+		let testedText = "tested"
+		multipart.addPart(named: "Text", string: testedText)
+		multipart.addPart(named: "File1", stream: arbitraryStream, streamFilename: "text.txt", streamLength: arbitraryData.count)
+		let (fileURL, _) = try createTestFile()
+		multipart.addPart(named: "File2", fileURL: fileURL, contentType: "text/html")
+
+		var readCount = 0
+		var finalData = Data()
+		for _ in 0..<100 {
+			let bufferSize = 20
+			let testPoint = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+			let buffer = UnsafeMutableBufferPointer<UInt8>(start: testPoint, count: bufferSize)
+			buffer.initialize(repeating: 0)
+
+			readCount += multipart.read(testPoint, maxLength: bufferSize)
+
+			let data = Data(buffer: buffer)
+
+			finalData += data
+		}
+
+		let expected = """
+		--Boundary-alskdglkasdjfglkajsdf\r\nContent-Disposition: form-data; name=\"Text\"\r\n\r\ntested\r\n--Boundary-\
+		alskdglkasdjfglkajsdf\r\nContent-Disposition: form-data; name=\"File1\"; filename=\"text.txt\"\r\nContent-Type: \
+		application/octet-stream\r\n\r\nOdd input stream\r\n--Boundary-alskdglkasdjfglkajsdf\r\nContent-Disposition: \
+		form-data; name=\"File2\"; filename=\"tempfile\"\r\nContent-Type: text/html\r\n\r\n<html><body>this is a \
+		body</body></html>\r\n
+		"""
+
+		finalData = finalData[0..<readCount]
+		let finalString = String(data: finalData, encoding: .utf8)
+		XCTAssertEqual(expected, finalString)
+	}
+
 	func testMultipartUpload() throws {
 		let networkHandler = generateNetworkHandlerInstance()
 
