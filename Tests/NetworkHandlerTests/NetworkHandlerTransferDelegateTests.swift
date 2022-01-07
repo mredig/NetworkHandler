77f4a0c55b6c611b4d04733b05eb7a3be5d74003
Michael Redig <mredig@gmail.com>
2022-01-06 23:03:56 -0600

    (feat) test network handler transfer delegate functions

--- Tests/NetworkHandlerTests/NetworkHandlerTransferDelegateTests.swift
+++ Tests/NetworkHandlerTests/NetworkHandlerTransferDelegateTests.swift
@@ -58,83 +58,77 @@ class NetworkHandlerTransferDelegateTests: NetworkHandlerBaseTest {
 	}
 
 	/// tests progress tracking when uploading - will fail without api/secret for wasabi in environment
-//	func testUploadProgress() {
-//		let networkHandler = generateNetworkHandlerInstance()
-//
-//		let url = URL(string: "https://s3.wasabisys.com/network-handler-tests/uploader.bin")!
-//		var request = url.request
-//		let method = HTTPMethod.put
-//		request.httpMethod = method
-//
-//		let formatter = DateFormatter()
-//		formatter.locale = Locale(identifier: "en_US_POSIX")
-//		formatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss Z"
-//
-//		let now = Date()
-//
-//		// this can be changed per run depending on internet variables - large enough to take more than an instant,
-//		// small enough to not timeout.
-//		let sizeOfUploadMB = 10
-//
-//		let tempFile = FileManager.default.temporaryDirectory.appendingPathComponent("tempfile")
-//		let outputStream = OutputStream(url: tempFile, append: false)
-//		outputStream?.open()
-//		let length = 1024 * sizeOfUploadMB
-//		let gibberish = UnsafeMutablePointer<UInt8>.allocate(capacity: length)
-//
-//		(0..<1024).forEach { _ in
-//			(0..<length).forEach {
-//				gibberish[$0] = UInt8.random(in: 0...UInt8.max)
-//			}
-//
-//			outputStream?.write(gibberish, maxLength: length)
-//		}
-//		outputStream?.close()
-//		gibberish.deallocate()
-//
-//		let inputStream = InputStream(url: tempFile)
-//		addTeardownBlock {
-//			try? FileManager.default.removeItem(at: tempFile)
-//		}
-//
-//		let string = "\(method.rawValue)\n\n\n\(formatter.string(from: now))\n\(url.path)"
-//		let signature = string.hmac(algorithm: .sha1, key: TestEnvironment.s3AccessSecret)
-//
-//
-//		request.addValue("\(formatter.string(from: now))", forHTTPHeaderField: .date)
-//		request.addValue("AWS \(TestEnvironment.s3AccessKey):\(signature)", forHTTPHeaderField: .authorization)
-//		request.httpBodyStream = inputStream
-//
-//		let waitForMocking = expectation(description: "Wait for mocking")
-//		let handle = networkHandler.transferMahDatas(with: request) { result in
-//			defer { waitForMocking.fulfill() }
-//			if case .failure(let error) = result {
-//				XCTFail("Upload failed: \(error)")
-//			}
-//		}
-//
-//
-//		var progressTracker: [Double] = []
-//		handle.onProgressUpdated { task in
-//			progressTracker.append(task.progress.fractionCompleted)
-//		}
-//
-//		wait(for: [waitForMocking], timeout: 30)
-//
-//		XCTAssertGreaterThan(progressTracker.count, 2)
-//
-//		let progressDeltaTracker = progressTracker
-//			.reduce(into: [(Double, Double)]() ) {
-//				let lastValue = $0.last?.0 ?? 0
-//				$0.append(($1, $1 - lastValue))
-//			}
-//
-//		let averageDelta = progressDeltaTracker
-//			.map(\.1)
-//			.reduce(0, +) / Double(progressTracker.count)
-//
-//		XCTAssertGreaterThan(averageDelta, 0)
-//	}
+	func testUploadProgress() async throws {
+		let networkHandler = generateNetworkHandlerInstance(mockedDefaultSession: false)
+
+		let url = URL(string: "https://s3.wasabisys.com/network-handler-tests/uploader.bin")!
+		var request = url.request
+		let method = HTTPMethod.put
+		request.httpMethod = method
+
+		let formatter = DateFormatter()
+		formatter.locale = Locale(identifier: "en_US_POSIX")
+		formatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss Z"
+
+		let now = Date()
+
+		// this can be changed per run depending on internet variables - large enough to take more than an instant,
+		// small enough to not timeout.
+		let sizeOfUploadMB = 10
+
+		let tempFile = FileManager.default.temporaryDirectory.appendingPathComponent("tempfile")
+		let outputStream = OutputStream(url: tempFile, append: false)
+		outputStream?.open()
+		let length = 1024 * sizeOfUploadMB
+		let gibberish = UnsafeMutablePointer<UInt8>.allocate(capacity: length)
+
+		(0..<1024).forEach { _ in
+			(0..<length).forEach {
+				gibberish[$0] = UInt8.random(in: 0...UInt8.max)
+			}
+
+			outputStream?.write(gibberish, maxLength: length)
+		}
+		outputStream?.close()
+		gibberish.deallocate()
+
+		let inputStream = InputStream(url: tempFile)
+		addTeardownBlock {
+			try? FileManager.default.removeItem(at: tempFile)
+		}
+
+		let string = "\(method.rawValue)\n\n\n\(formatter.string(from: now))\n\(url.path)"
+		let signature = string.hmac(algorithm: .sha1, key: TestEnvironment.s3AccessSecret)
+
+
+		request.addValue("\(formatter.string(from: now))", forHTTPHeaderField: .date)
+		request.addValue("AWS \(TestEnvironment.s3AccessKey):\(signature)", forHTTPHeaderField: .authorization)
+		request.httpBodyStream = inputStream
+
+		var progressTracker: [Double] = []
+
+		let myDel = DownloadDelegate()
+		myDel.progressPub.sink {
+			progressTracker.append($0)
+			print($0)
+		}
+
+		try await networkHandler.transferMahDatas(for: request, delegate: myDel)
+
+		XCTAssertGreaterThan(progressTracker.count, 2)
+
+		let progressDeltaTracker = progressTracker
+			.reduce(into: [(Double, Double)]() ) {
+				let lastValue = $0.last?.0 ?? 0
+				$0.append(($1, $1 - lastValue))
+			}
+
+		let averageDelta = progressDeltaTracker
+			.map(\.1)
+			.reduce(0, +) / Double(progressTracker.count)
+
+		XCTAssertGreaterThan(averageDelta, 0)
+	}
 //
 //	func testOnTaskCompletion() {
 //		let networkHandler = generateNetworkHandlerInstance()
