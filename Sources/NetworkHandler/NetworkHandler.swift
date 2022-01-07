4d49a4d6a748e4f014de6e6235da0c8db5c20734
Michael Redig <mredig@gmail.com>
2022-01-07 17:50:17 -0600

    (refactor) dont need cache reset on nhactor

--- Sources/NetworkHandler/NetworkHandler.swift
+++ Sources/NetworkHandler/NetworkHandler.swift
@@ -44,7 +44,6 @@ public class NetworkHandler {
 		self.sessionDelegate = sessionDelegate
 	}
 
-	@NHActor
 	public func resetCache(memory: Bool = true, disk: Bool = true) {
 		cache.reset(memory: memory, disk: disk)
 	}
