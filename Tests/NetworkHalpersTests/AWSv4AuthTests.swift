77e4e26535499abfdaab42ba44f1e62e81bc2d00
Michael Redig <mredig@gmail.com>
2022-07-16 00:59:14 -0500

    (refactor) configured awsauth tests

--- Tests/NetworkHalpersTests/AWSv4AuthTests.swift
+++ Tests/NetworkHalpersTests/AWSv4AuthTests.swift
@@ -15,13 +15,15 @@ class AWSv4AuthTests: XCTestCase {
 			requestMethod: .get,
 			url: url,
 			date: date,
-			awsKey: "ASZUJQ6PHU62NDOW8Y2L",
-			awsSecret: "t2wWpJDvLmeDTwnzCdzyeAHNeCG0LbE80FR50XbV",
+			awsKey: "LCNRBZKKF8QEWNT2DYGM",
+			awsSecret: "F2XxYE7h6zim2nCgNaUqZp9hGWqYzy7kbNMazR8g",
 			awsRegion: .usWest1,
 			awsService: .s3,
 			hexContentHash: SHA256.hash(data: Data("".utf8)).hex(),
 			additionalSignedHeaders: [:])
 
-		print(info)
+		XCTAssertEqual("AWS4-HMAC-SHA256\n2022-07-15T06:43:24Z\n20220715/us-west-1/s3/aws4_request\ncc388e661c394a9b73dd2a71d1a20dd890afb1433cb704549016be6a2af18cc5", info.stringToSign)
+		XCTAssertEqual("d3465b2bb220cf97a135c0746047bda485e70295f513048c192ca88ff50ecd18", info.signature)
+		XCTAssertEqual("AWS4-HMAC-SHA256 Credential=LCNRBZKKF8QEWNT2DYGM/20220715/us-west-1/s3/aws4_request,SignedHeaders=host;x-amz-content-sha256,Signature=d3465b2bb220cf97a135c0746047bda485e70295f513048c192ca88ff50ecd18", info.authorizationString)
 	}
 }
