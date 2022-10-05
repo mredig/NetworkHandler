3b6ddc9ee575d7026b8e60d678ba840aa9226952
Michael Redig <mredig@gmail.com>
2022-10-05 16:58:00 -0500

    (nit) bump safer continuation requirement

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
 		.package(url: "https://github.com/apple/swift-crypto.git", .upToNextMinor(from: "2.0.0")),
 		.package(url: "https://github.com/mredig/SaferContinuation.git", .upToNextMinor(from: "1.1.0")),
-		.package(url: "https://github.com/KnowMeGit/Swiftwood.git", .upToNextMinor(from: "0.1.0")),
+		.package(url: "https://github.com/KnowMeGit/Swiftwood.git", .upToNextMinor(from: "0.1.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
