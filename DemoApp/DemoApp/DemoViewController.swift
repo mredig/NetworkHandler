d16ae4d7977cd4e263598a004630f61e4ad10a50
Michael Redig <mredig@gmail.com>
2022-10-08 13:54:33 -0500

    (refactor) kinda fixed the demo app, but still broken
    so progress i guess

--- DemoApp/DemoApp/DemoViewController.swift
+++ DemoApp/DemoApp/DemoViewController.swift
@@ -13,7 +13,7 @@ class DemoViewController: UITableViewController {
 
 	// MARK: - Properties
 	let demoModelController = DemoModelController()
-	private var tasks = [UITableViewCell: NetworkLoadingTask]()
+	private var tasks = [UITableViewCell: Task<Void, Error>]()
 
 	// MARK: - Outlets
 	@IBOutlet private var generateDemoDataButton: UIButton!
@@ -21,30 +21,44 @@ class DemoViewController: UITableViewController {
 	// MARK: - Actions
 	@IBAction func generateDemoDataButtonPressed(_ sender: UIButton) {
 		sender.isEnabled = false
-		demoModelController.generateDemoData { [weak self] in
-			self?.demoModelController.fetchDemoModels(completion: { [weak self] error in
-				if let error = error {
-					NSLog("There was an error \(error)")
-				}
-				DispatchQueue.main.async {
-					self?.tableView.reloadData()
-					sender.isEnabled = true
-				}
-			})
+//		demoModelController.generateDemoData { [weak self] in
+//			self?.demoModelController.fetchDemoModels(completion: { [weak self] error in
+//				if let error = error {
+//					NSLog("There was an error \(error)")
+//				}
+//				DispatchQueue.main.async {
+//					self?.tableView.reloadData()
+//					sender.isEnabled = true
+//				}
+//			})
+//		}
+		Task {
+			do {
+				try await demoModelController.generateDemoData()
+			} catch {
+				print("Error generating data: \(error)")
+			}
+			refreshData()
+			sender.isEnabled = true
 		}
 	}
 
 	@objc func refreshData() {
-		tableView.refreshControl?.beginRefreshing()
-		demoModelController.fetchDemoModels { [weak self] error in
-			DispatchQueue.main.async {
-				if let error = error {
-					let alert = UIAlertController(error: error)
-					self?.present(alert, animated: true)
-				}
-				self?.tableView.refreshControl?.endRefreshing()
-				self?.tableView.reloadData()
+		Task {
+			do {
+				try await demoModelController.fetchDemoModels()
+			} catch {
+				let alertVC = UIAlertController(title: "Error", message: "Error loading data: \(error)", preferredStyle: .alert)
+
+				let button = UIAlertAction(title: "Okay", style: .default, handler: nil)
+
+				alertVC.addAction(button)
+
+				present(alertVC, animated: true)
 			}
+
+			tableView.refreshControl?.endRefreshing()
+			tableView.reloadData()
 		}
 	}
 
@@ -88,8 +102,10 @@ extension DemoViewController {
 	override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
 		if editingStyle == .delete {
 			let demoModel = demoModelController.demoModels[indexPath.row]
-			demoModelController.delete(model: demoModel)
-			tableView.deleteRows(at: [indexPath], with: .automatic)
+			Task {
+				try await demoModelController.delete(model: demoModel)
+				tableView.deleteRows(at: [indexPath], with: .automatic)
+			}
 		}
 	}
 
@@ -100,26 +116,50 @@ extension DemoViewController {
 
 		let demoModel = demoModelController.demoModels[indexPath.row]
 
-		tasks[cell] = NetworkHandler.default.transferMahDatas(
-			with: demoModel.imageURL.request,
-			usingCache: true,
-			completion: { [weak self] (result: Result<Data, Error>) in
-				DispatchQueue.main.async {
-					do {
-						let imageData = try result.get()
-						cell.imageView?.image = UIImage(data: imageData)
-						cell.layoutSubviews()
-						self?.tasks[cell] = nil
-					} catch {
-						if case NetworkError.otherError(error: let otherError) = error {
-							if (otherError as NSError).code == -999 {
-								// cancelled
-								return
-							}
-						}
-						NSLog("error loading image from url '\(demoModel.imageURL)': \(error)")
+		tasks[cell] = Task {
+			do {
+				let (data, _) = try await NetworkHandler.default.transferMahDatas(
+					for: demoModel.imageURL.request,
+					delegate: nil,
+					usingCache: true,
+					sessionConfiguration: nil)
+
+				try Task.checkCancellation()
+				cell.imageView?.image = UIImage(data: data)
+				cell.layoutSubviews()
+				tasks[cell] = nil
+			} catch {
+				if case NetworkError.otherError(error: let otherError) = error {
+					if (otherError as NSError).code == -999 {
+						// cancelled
+						return
 					}
 				}
-			})
+				print("Error loading image from url: '\(demoModel.imageURL)': \(error)")
+			}
+
+
+		}
+//		tasks[cell] = NetworkHandler.default.transferMahDatas(
+//			with: demoModel.imageURL.request,
+//			usingCache: true,
+//			completion: { [weak self] (result: Result<Data, Error>) in
+//				DispatchQueue.main.async {
+//					do {
+//						let imageData = try result.get()
+//						cell.imageView?.image = UIImage(data: imageData)
+//						cell.layoutSubviews()
+//						self?.tasks[cell] = nil
+//					} catch {
+//						if case NetworkError.otherError(error: let otherError) = error {
+//							if (otherError as NSError).code == -999 {
+//								// cancelled
+//								return
+//							}
+//						}
+//						NSLog("error loading image from url '\(demoModel.imageURL)': \(error)")
+//					}
+//				}
+//			})
 	}
 }
