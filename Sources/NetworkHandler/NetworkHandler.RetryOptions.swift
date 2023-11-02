e0229018b5cb031b525ebf83899e45c30c5021cc
Michael Redig <mredig@gmail.com>
2023-11-02 00:22:31 -0500

    (refactor) retry options in new file

---
+++ Sources/NetworkHandler/NetworkHandler.RetryOptions.swift
@@ -0,0 +1,54 @@
+import Foundation
+
+extension NetworkHandler {
+	public struct RetryConfiguration {
+		public static let simple = RetryConfiguration(delay: 0)
+
+		public var delay: TimeInterval
+		public var updatedRequest: NetworkRequest?
+
+		public init(
+			delay: TimeInterval,
+			updatedRequest: NetworkRequest? = nil
+		) {
+			self.delay = delay
+			self.updatedRequest = updatedRequest
+		}
+	}
+
+	public struct DefaultReturnValueConfiguration {
+		public var data: Data
+		public var response: ResponseOption
+
+		public enum ResponseOption {
+			case full(HTTPURLResponse)
+			case code(Int)
+		}
+	}
+
+	public enum RetryOption {
+		public static let retry = RetryOption.retryWithConfiguration(config: .simple)
+		case retryWithConfiguration(config: RetryConfiguration)
+		public static func retry(
+			withDelay delay: TimeInterval = 0,
+			updatedRequest: NetworkRequest? = nil
+		) -> RetryOption {
+			let config = RetryConfiguration(delay: delay, updatedRequest: updatedRequest)
+			return .retryWithConfiguration(config: config)
+		}
+
+		case `throw`(updatedError: Error?)
+		public static let `throw` = RetryOption.throw(updatedError: nil)
+		case defaultReturnValue(config: DefaultReturnValueConfiguration)
+
+		public static func defaultReturnValue(data: Data, statusCode: Int) -> RetryOption {
+			let config = DefaultReturnValueConfiguration(data: data, response: .code(statusCode))
+			return .defaultReturnValue(config: config)
+		}
+
+		public static func defaultReturnValue(data: Data, urlResponse: HTTPURLResponse) -> RetryOption {
+			let config = DefaultReturnValueConfiguration(data: data, response: .full(urlResponse))
+			return .defaultReturnValue(config: config)
+		}
+	}
+}
--- Sources/NetworkHandler/NetworkHandler.swift
+++ Sources/NetworkHandler/NetworkHandler.swift
@@ -221,57 +221,6 @@ public class NetworkHandler {
 		throw NetworkError.unspecifiedError(reason: "Escaped while loop")
 	}
 
-	public struct RetryConfiguration {
-		public static let retry = RetryConfiguration(delay: 0)
-
-		public var delay: TimeInterval
-		public var updatedRequest: NetworkRequest?
-
-		public init(
-			delay: TimeInterval,
-			updatedRequest: NetworkRequest? = nil
-		) {
-			self.delay = delay
-			self.updatedRequest = updatedRequest
-		}
-	}
-
-	public struct DefaultReturnValueConfiguration {
-		public var data: Data
-		public var response: ResponseOption
-
-		public enum ResponseOption {
-			case full(HTTPURLResponse)
-			case code(Int)
-		}
-	}
-
-	public enum RetryOption {
-		public static let retry = RetryOption.retryWithConfiguration(config: .retry)
-		case retryWithConfiguration(config: RetryConfiguration)
-		public static func retry(
-			withDelay delay: TimeInterval = 0,
-			updatedRequest: NetworkRequest? = nil
-		) -> RetryOption {
-			let config = RetryConfiguration(delay: delay, updatedRequest: updatedRequest)
-			return .retryWithConfiguration(config: config)
-		}
-
-		case `throw`(updatedError: Error?)
-		public static let `throw` = RetryOption.throw(updatedError: nil)
-		case defaultReturnValue(config: DefaultReturnValueConfiguration)
-
-		public static func defaultReturnValue(data: Data, statusCode: Int) -> RetryOption {
-			let config = DefaultReturnValueConfiguration(data: data, response: .code(statusCode))
-			return .defaultReturnValue(config: config)
-		}
-
-		public static func defaultReturnValue(data: Data, urlResponse: HTTPURLResponse) -> RetryOption {
-			let config = DefaultReturnValueConfiguration(data: data, response: .full(urlResponse))
-			return .defaultReturnValue(config: config)
-		}
-	}
-
 	@NHActor
 	@discardableResult private func _transferMahDatas(
 		for request: NetworkRequest,
