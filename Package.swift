518f084f0256e16ff05f287b6964bcf317701087
Michael Redig <mredig@gmail.com>
2023-11-02 02:41:19 -0500

    (feat) added pizza macros

--- Package.swift
+++ Package.swift
@@ -29,6 +29,7 @@ let package = Package(
 		.package(url: "https://github.com/apple/swift-crypto.git", .upToNextMinor(from: "2.0.0")),
 		.package(url: "https://github.com/mredig/SaferContinuation.git", .upToNextMinor(from: "1.3.0")),
 		.package(url: "https://github.com/KnowMeGit/Swiftwood.git", .upToNextMinor(from: "0.3.0")),
+		.package(url: "https://github.com/mredig/PizzaMacros.git", .upToNextMinor(from: "0.1.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -65,6 +66,10 @@ let package = Package(
             dependencies: ["NetworkHandler", "TestSupport", "Swizzles"]),
 		.testTarget(
 			name: "NetworkHalpersTests",
-			dependencies: ["NetworkHalpers", "TestSupport"]),
+			dependencies: [
+				"NetworkHalpers",
+				"TestSupport",
+				"PizzaMacros",
+			]),
     ]
 )
