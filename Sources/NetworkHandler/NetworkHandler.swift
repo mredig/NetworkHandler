fb847ef4577eb7eb74f8c5e6dd4e9a421965e7d6
Michael Redig <mredig@gmail.com>
2025-03-03 05:09:21 -0600

    (refactor) NetworkHandler cleanup

--- Sources/NetworkHandler/NetworkHandler.swift
+++ Sources/NetworkHandler/NetworkHandler.swift
@@ -16,6 +16,7 @@ public class NetworkHandler<Engine: NetworkEngine> {
 	public var enableLogging = false {
 		didSet {
 			guard enableLogging else { return }
+			// FIXME: DO logging
 			setupLogging()
 		}
 	}
@@ -28,30 +29,14 @@ public class NetworkHandler<Engine: NetworkEngine> {
 
 	public let name: String
 
-	/// A default instance of NetworkHandler provided for convenience. Use is optional.
-	//	public static let `default` = NetworkHandler(name: "NHDefault", diskCacheCapacity: .max)
-
 	/// Underlying engine running network transactions
 	public let engine: Engine
-	private let nhMainUploadDelegate: NHUploadDelegate
-	//	private let delegateQueue: OperationQueue = {
-	//		let queue = OperationQueue()
-	//		queue.maxConcurrentOperationCount = 1
-	//		return queue
-	//	}()
-	private let atomicPropertyLock = MutexLock()
 
 	// MARK: - Lifecycle
 	/// Initialize a new NetworkHandler instance.
 	public init(name: String, engine: Engine, diskCacheCapacity: UInt64 = .max) {
 		self.name = name
 		self.cache = NetworkCache(name: "\(name)-Cache", diskCacheCapacity: diskCacheCapacity)
-
-		//		let config = configuration ?? .networkHandlerDefault
-
-		let uploadDelegate = NHUploadDelegate()
-		self.nhMainUploadDelegate = uploadDelegate
-		//		self.defaultSession = URLSession(configuration: config, delegate: uploadDelegate, delegateQueue: delegateQueue)
 
 		self.engine = engine
 	}
@@ -65,12 +50,12 @@ public class NetworkHandler<Engine: NetworkEngine> {
 	}
 
 	// MARK: - Network Handling
-	public enum PollContinuation<T> {
+	public enum PollContinuation<T: Sendable>: Sendable {
 		case finish(PollResult<T>)
 		case `continue`(NetworkRequest, TimeInterval)
 	}
 
-	public typealias PollResult<T> = Result<(EngineResponseHeader, T), Error>
+	public typealias PollResult<T: Sendable> = Result<(EngineResponseHeader, T), Error>
 	/// Immediately sends request, then can have a delay before repeating (or modifying) the request via the return value
 	/// of the `until` block.
 	///
@@ -82,7 +67,6 @@ public class NetworkHandler<Engine: NetworkEngine> {
 		delegate: NetworkHandlerTaskDelegate? = nil,
 		usingCache cacheOption: NetworkHandler.CacheKeyOption = .dontUseCache,
 		decoder: NHDecoder = DownloadEngineRequest.defaultDecoder,
-		//		sessionConfiguration: URLSessionConfiguration? = nil,
 		until: @escaping @NHActor (NetworkRequest, PollResult<T>) async throws -> PollContinuation<T>
 	) async throws -> (responseHeader: EngineResponseHeader, result: T) {
 		func doPoll(request: NetworkRequest) async -> PollResult<T> {
@@ -92,7 +76,7 @@ public class NetworkHandler<Engine: NetworkEngine> {
 					for: request,
 					delegate: delegate,
 					usingCache: cacheOption)
-				let decoded: T = try decodeData(from: request, data: data, using: decoder)
+				let decoded: T = try decodeData(data: data, using: decoder)
 				polledResult = .success((header, decoded))
 			} catch {
 				polledResult = .failure(error)
@@ -130,7 +114,6 @@ public class NetworkHandler<Engine: NetworkEngine> {
 	 - request: NetworkRequest containing the url and other request information.
 	 - cacheOption: NetworkHandler.CacheKeyOption indicating whether to use cache with or without a key overrride or not
 	 at all. **Default**: `.dontUseCache`
-	 - session: URLSession instance. **Default**: `self.defaultSession`
 	 - Returns: The resulting, decoded data safely typed as the `DecodableType` and the `URLResponse` from the task
 	 */
 	@NHActor
@@ -139,7 +122,6 @@ public class NetworkHandler<Engine: NetworkEngine> {
 		delegate: NetworkHandlerTaskDelegate? = nil,
 		usingCache cacheOption: NetworkHandler.CacheKeyOption = .dontUseCache,
 		decoder: NHDecoder = DownloadEngineRequest.defaultDecoder,
-		//		sessionConfiguration: URLSessionConfiguration? = nil,
 		onError: @escaping RetryOptionBlock<Data> = { _, _, _ in .throw }
 	) async throws -> (responseHeader: EngineResponseHeader, decoded: DecodableType) {
 		let (header, rawData) = try await transferMahDatas(
@@ -148,7 +130,7 @@ public class NetworkHandler<Engine: NetworkEngine> {
 			usingCache: cacheOption,
 			onError: onError)
 
-		return try (header, decodeData(from: request, data: rawData, using: decoder))
+		return try (header, decodeData(data: rawData, using: decoder))
 	}
 
 	/**
@@ -156,7 +138,6 @@ public class NetworkHandler<Engine: NetworkEngine> {
 	 - request: NetworkRequest containing the url and other request information.
 	 - cacheOption: NetworkHandler.CacheKeyOption indicating whether to use cache with or without a key overrride or not
 	 at all. **Default**: `.dontUseCache`
-	 - session: URLSession instance. **Default**: `self.defaultSession`
 	 - Returns: The resulting,  raw data typed as `Data` and the `URLResponse` from the task
 
 	 Note that delegate is only valid in iOS 15, macOS 12, tvOS 15, and watchOS 8 and higher
@@ -166,7 +147,6 @@ public class NetworkHandler<Engine: NetworkEngine> {
 		for request: NetworkRequest,
 		delegate: NetworkHandlerTaskDelegate? = nil,
 		usingCache cacheOption: NetworkHandler.CacheKeyOption = .dontUseCache,
-		//		sessionConfiguration: URLSessionConfiguration? = nil,
 		onError: @escaping RetryOptionBlock<Data> = { _, _, _ in .throw }
 	) async throws -> (responseHeader: EngineResponseHeader, data: Data) {
 		// FIXME: Do cache option
@@ -245,96 +225,6 @@ public class NetworkHandler<Engine: NetworkEngine> {
 		return (httpResponse, bodyResponseStream)
 	}
 
-
-	@NHActor
-	@discardableResult private func _streamMahDatas(
-		for request: NetworkRequest,
-		delegate: NetworkHandlerTaskDelegate? = nil,
-		usingCache cacheOption: NetworkHandler.CacheKeyOption = .dontUseCache,
-		attempt: Int
-//		sessionConfiguration: URLSessionConfiguration? = nil
-	) async throws -> NHRawResponse {
-		if let cacheKey = cacheOption.cacheKey(url: request.url) {
-			if let cachedData = cache[cacheKey] {
-				return (.data(cachedData.data), cachedData.response)
-			}
-		}
-
-//		let session = sessionConfiguration
-//			.map { URLSession(configuration: $0, delegate: nhMainUploadDelegate, delegateQueue: delegateQueue) }
-//		?? defaultSession
-
-//		defer {
-//			if session !== defaultSession {
-//				session.finishTasksAndInvalidate()
-//			}
-//		}
-
-		let (httpResponse, bodyResponseStream): (EngineResponseHeader, Engine.ResponseBodyStream)
-		do {
-			switch request {
-			case .upload(let uploadRequest, let payload):
-				let (sendProgress, responseTask, bodyStream) = try await engine.uploadNetworkData(request: uploadRequest, with: payload)
-//				(data, httpResponse) = try await uploadTask(
-//					session: session,
-//					request: request,
-//					uploadFile: uploadFile,
-//					delegate: delegate)
-				async let progressBlock: Void = { @NHActor in
-					var signaledStart = false
-					for try await count in sendProgress {
-						if signaledStart == false {
-							signaledStart = true
-							delegate?.transferDidStart(for: request)
-						}
-						delegate?.sentData(for: request, byteCountSent: Int(count), totalExpectedToSend: nil)
-					}
-				}()
-				async let responseHeader = responseTask.value
-
-				try await progressBlock
-				httpResponse = try await responseHeader
-				delegate?.responseHeaderRetrieved(for: request, header: httpResponse)
-				bodyResponseStream = bodyStream
-			case .download(let downloadRequest):
-				let (header, bodyStream) = try await engine.fetchNetworkData(from: downloadRequest)
-				httpResponse = header
-				bodyResponseStream = bodyStream
-			}
-		} catch {
-			throw error.convertToNetworkErrorIfCancellation()
-		}
-
-		guard request.expectedResponseCodes.rawValue.contains(httpResponse.status) else {
-			logIfEnabled(
-				"""
-				Error: Server replied with expected status code: Got \(httpResponse.status) \
-				expected \(request.expectedResponseCodes)
-				""",
-				logLevel: .error)
-			let data: Data? = await {
-				var accumulator = Data()
-				do {
-					for try await bytes in bodyResponseStream {
-						guard accumulator.count < 1024 * 1024 * 10 else { break }
-						accumulator.append(contentsOf: bytes)
-					}
-				} catch {
-					return accumulator.isOccupied ? accumulator : nil
-				}
-				return accumulator.isOccupied ? accumulator : nil
-			}()
-			throw NetworkError.httpUnexpectedStatusCode(code: httpResponse.status, originalRequest: request, data: data)
-		}
-
-		// FIXME: something here
-//		if let cacheKey = cacheOption.cacheKey(url: request.url) {
-//			self.cache[cacheKey] = NetworkCacheItem(response: httpResponse, data: data)
-//		}
-
-		return (bodyResponseStream, httpResponse)
-	}
-
 	private func retryHandler<T>(
 		originalRequest: NetworkRequest,
 		transferTask: (_ request: NetworkRequest, _ attempt: Int) async throws -> (EngineResponseHeader, T),
@@ -380,7 +270,7 @@ public class NetworkHandler<Engine: NetworkEngine> {
 		throw NetworkError.unspecifiedError(reason: "Escaped while loop")
 	}
 
-	private func decodeData<DecodableType: Decodable>(from request: NetworkRequest, data: Data, using decoder: NHDecoder) throws -> DecodableType {
+	private func decodeData<DecodableType: Decodable>(data: Data, using decoder: NHDecoder) throws -> DecodableType {
 		guard DecodableType.self != Data.self else {
 			return data as! DecodableType // swiftlint:disable:this force_cast
 		}
@@ -395,152 +285,6 @@ public class NetworkHandler<Engine: NetworkEngine> {
 			throw NetworkError.dataCodingError(specifically: error, sourceData: data)
 		}
 	}
-
-	private var _taskHolders: [UUID: URLSessionTask] = [:]
-	package var taskHolders: [UUID: URLSessionTask] {
-		get {
-			atomicPropertyLock.withLock { _taskHolders }
-		}
-		set {
-			atomicPropertyLock.withLock {
-				_taskHolders = newValue
-			}
-		}
-	}
-//	private func downloadTask(
-//		session: URLSession,
-//		request: NetworkRequest,
-//		delegate: NetworkHandlerTaskDelegate?
-//	) async throws -> (Data, HTTPURLResponse) {
-//		let (asyncBytes, response) = try await session.bytes(for: request.urlRequest, delegate: delegate)
-//		let task = asyncBytes.task
-//		let taskID = UUID()
-//		taskHolders[taskID] = task
-//		defer { taskHolders[taskID] = nil }
-//
-//		return try await withTaskCancellationHandler(operation: {
-//			task.priority = request.priority.rawValue
-//			delegate?.task = task
-//
-//
-//			guard let httpResponse = response as? HTTPURLResponse else {
-//				logIfEnabled("Error: Server replied with no status code", logLevel: .error)
-//				throw NetworkError.noStatusCodeResponse
-//			}
-//
-//			if let delegate {
-//				await MainActor.run {
-//					delegate.networkHandlerTaskDidStart(task)
-//				}
-//			}
-//
-//			var data = Data()
-//			data.reserveCapacity(Int(httpResponse.expectedContentLength))
-//			var lastUpdate = Date.distantPast
-//			var count = 0
-//			for try await byte in asyncBytes {
-//				data.append(byte)
-//				count += 1
-//
-//				let now = Date()
-//				if now > lastUpdate.addingTimeInterval(1 / 30) {
-//					lastUpdate = now
-//
-//					delegate?.networkHandlerTask(task, didProgress: Double(count) / Double(httpResponse.expectedContentLength))
-//				}
-//			}
-//
-//			return (data, httpResponse)
-//		}, onCancel: { [weak self, taskID] in
-//			let task = self?.taskHolders.removeValue(forKey: taskID)
-//			task?.cancel()
-//		})
-//	}
-
-//	private func uploadTask(
-//		session: URLSession,
-//		request: NetworkRequest,
-//		uploadFile: NetworkRequest.UploadFile,
-//		delegate: NetworkHandlerTaskDelegate?
-//	) async throws -> (Data, HTTPURLResponse) {
-//		let taskID = UUID()
-//
-//		return try await withTaskCancellationHandler(
-//			operation: {
-//				let data: Data
-//				let task: URLSessionUploadTask
-//				switch uploadFile {
-//				case .localFile(let url):
-//					task = session.uploadTask(with: request.urlRequest, fromFile: url)
-//				case .data(let uploadData):
-//					task = session.uploadTask(with: request.urlRequest, from: uploadData)
-//				case .inputStream(let inputStream):
-//					task = session.uploadTask(withStreamedRequest: request.urlRequest)
-//					nhMainUploadDelegate.addInputStream(inputStream, for: task)
-//					task.delegate = nhMainUploadDelegate
-//				}
-//
-//				if let delegate {
-//					nhMainUploadDelegate.addTaskDelegate(delegate, for: task)
-//				}
-//				
-//				taskHolders[taskID] = task
-//
-//				data = try await withCheckedThrowingContinuation({ continuation in
-//					let safer = SaferContinuation(
-//						continuation,
-//						isFatal: [.onMultipleCompletions, .onPostRunDelayCheck],
-//						timeout: (request.timeoutInterval * 1.5) + 0.5,
-//						delayCheckInterval: 3,
-//						context: "(\(request.httpMethod as Any)): \(request.url as Any)")
-//
-//					var dataAccumulator = Data()
-//
-//					nhMainUploadDelegate
-//						.taskKeepalivePublisher(for: task)
-//						.sink(receiveValue: {
-//							safer.keepAlive()
-//						})
-//
-//					nhMainUploadDelegate
-//						.dataPublisher(for: task)
-//						.sink(
-//							receiveValue: { data in
-//								dataAccumulator.append(contentsOf: data)
-//								safer.keepAlive()
-//							},
-//							receiveCompletion: { completion in
-//								switch completion {
-//								case .finished:
-//									safer.resume(returning: dataAccumulator)
-//								case .failure(let error):
-//									safer.resume(throwing: error)
-//								}
-//							})
-//
-//					task.resume()
-//
-//					DispatchQueue.main.async {
-//						delegate?.networkHandlerTaskDidStart(task)
-//					}
-//				})
-//
-//				guard let response = task.response else {
-//					logIfEnabled("Error no response provided", logLevel: .error)
-//					throw NetworkError.noURLResponse
-//				}
-//				guard let httpResponse = response as? HTTPURLResponse else {
-//					logIfEnabled("Error: Server replied with no status code", logLevel: .error)
-//					throw NetworkError.noStatusCodeResponse
-//				}
-//
-//				return (data, httpResponse)
-//			},
-//			onCancel: { [weak self, taskID] in
-//				let task = self?.taskHolders.removeValue(forKey: taskID)
-//				task?.cancel()
-//			})
-//	}
 
 	private func logIfEnabled(_ string: String, logLevel: Swiftwood.Level) {
 		if enableLogging {
@@ -579,21 +323,21 @@ public class NetworkHandler<Engine: NetworkEngine> {
 	}
 }
 
-extension AsyncCancellableThrowingStream {
-	static func data(_ data: Data, chunkSize: Int = 1024 * 1024 * 4) -> AsyncCancellableThrowingStream<[UInt8], Error> {
-		AsyncCancellableThrowingStream<[UInt8], Error> { continuation in
-			var lastOffset = 0
-			for offset in stride(from: data.startIndex, through: data.endIndex, by: chunkSize) {
-				defer { lastOffset = offset }
-				guard
-					(lastOffset..<offset).isOccupied
-				else { continue }
-				try! continuation.yield(Array(data[lastOffset..<offset]))
-			}
-			if lastOffset < data.endIndex {
-				try! continuation.yield(Array(data[lastOffset..<data.endIndex]))
-			}
-			try? continuation.finish()
-		}
-	}
-}
+//extension AsyncCancellableThrowingStream {
+//	static func data(_ data: Data, chunkSize: Int = 1024 * 1024 * 4) -> AsyncCancellableThrowingStream<[UInt8], Error> {
+//		AsyncCancellableThrowingStream<[UInt8], Error> { continuation in
+//			var lastOffset = 0
+//			for offset in stride(from: data.startIndex, through: data.endIndex, by: chunkSize) {
+//				defer { lastOffset = offset }
+//				guard
+//					(lastOffset..<offset).isOccupied
+//				else { continue }
+//				try! continuation.yield(Array(data[lastOffset..<offset]))
+//			}
+//			if lastOffset < data.endIndex {
+//				try! continuation.yield(Array(data[lastOffset..<data.endIndex]))
+//			}
+//			try? continuation.finish()
+//		}
+//	}
+//}
