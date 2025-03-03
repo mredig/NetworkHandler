208e94a98910655dce16a13e22791069e807e552
Michael Redig <mredig@gmail.com>
2025-03-03 05:02:28 -0600

    (feat) add network handler task delegate

---
+++ Sources/NetworkHandler/NetworkHandlerTaskDelegate.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+@NHActor
+public protocol NetworkHandlerTaskDelegate {
+	func transferDidStart(for request: NetworkRequest)
+	func sentData(for request: NetworkRequest, byteCountSent: Int, totalExpectedToSend: Int?)
+	func sendingDataDidFinish(for request: NetworkRequest)
+	func responseHeaderRetrieved(for request: NetworkRequest, header: EngineResponseHeader)
+	func responseBodyReceived(for request: NetworkRequest, bytes: Data)
+	func responseBodyReceived(for request: NetworkRequest, byteCount: Int, totalExpectedToReceive: Int?)
+	func requestFinished(withError error: Error?)
+}
