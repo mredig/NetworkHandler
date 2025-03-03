dd3a07e59940ea1f8d6a9f976d2fe7f96ef41dc1
Michael Redig <mredig@gmail.com>
2025-03-03 05:01:37 -0600

    (refactor) network Error updates

--- Sources/NetworkHandler/NetworkError.swift
+++ Sources/NetworkHandler/NetworkError.swift
@@ -40,9 +40,9 @@ public enum NetworkError: Error, Equatable {
 	/**
 	Thrown when a `URLResponse` includes a response code other than 200, or a range
 	of 200-299 (depending on whether `strict200CodeResponse` is on or off). Wraps
-	the response code and included `Data?`, is there is any.
+	the response code and included `Data?`, if there is any.
 	*/
-	case httpNon200StatusCode(code: Int, originalRequest: NetworkRequest, data: Data?)
+	case httpUnexpectedStatusCode(code: Int, originalRequest: NetworkRequest, data: Data?)
 	/**
 	Not used within the NetworkHandler framework, but a preset error available for
 	use when there's an error with whatever database you're using. Wraps the
@@ -96,12 +96,12 @@ public enum NetworkError: Error, Equatable {
 			if case .dataCodingError(let rhsError, let rhsSourceData) = rhs,
 				otherError.localizedDescription == rhsError.localizedDescription,
 			lhsSourceData == rhsSourceData { return true } else { return false }
-		case .httpNon200StatusCode(code: let code, originalRequest: let request, data: let data):
+		case .httpUnexpectedStatusCode(code: let code, originalRequest: let request, data: let data):
 			if
-				case .httpNon200StatusCode(let rhsCode, let rhsRequest, let rhsData) = rhs,
+				case .httpUnexpectedStatusCode(let rhsCode, let rhsRequest, let rhsData) = rhs,
 				code == rhsCode,
 				data == rhsData,
-				request.urlRequest == rhsRequest.urlRequest {
+				request == rhsRequest {
 
 				return true
 			} else {
@@ -151,10 +151,10 @@ extension NetworkError: CustomDebugStringConvertible, LocalizedError {
 			return "NetworkError: Database Failure: (\(error))"
 		case .dataCodingError(specifically: let error, sourceData: let sourceData):
 			return "NetworkError: Data Coding Error\n Error: \(error)\nSourceData: \(stringifyData(sourceData))"
-		case .httpNon200StatusCode(code: let code, originalRequest: let request, data: let data):
+		case .httpUnexpectedStatusCode(code: let code, originalRequest: let request, data: let data):
 			let requestInfo = {
-				let method = request.httpMethod?.rawValue ?? ("UNKNOWN_METHOD")
-				let url = request.url?.absoluteString ?? "unknown url"
+				let method = request.method.rawValue
+				let url = request.url.absoluteString
 
 				var combined = "\(method)): \(url)"
 				if let requestID = request.requestID {
