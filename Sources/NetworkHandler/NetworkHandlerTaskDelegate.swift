af19586510cc84b9221b27d6096e54e585700a69
Michael Redig <mredig@gmail.com>
2025-03-05 19:10:01 -0600

    (refactor) clarify when delegate methods should be called

--- Sources/NetworkHandler/NetworkHandlerTaskDelegate.swift
+++ Sources/NetworkHandler/NetworkHandlerTaskDelegate.swift
@@ -2,11 +2,16 @@ import Foundation
 
 @NHActor
 public protocol NetworkHandlerTaskDelegate: Sendable {
+	/// Only called on uploads
 	func transferDidStart(for request: NetworkRequest)
+	/// Only called on uploads
 	func sentData(for request: NetworkRequest, byteCountSent: Int, totalExpectedToSend: Int?)
+	/// Only called on uploads
 	func sendingDataDidFinish(for request: NetworkRequest)
 	func responseHeaderRetrieved(for request: NetworkRequest, header: EngineResponseHeader)
+	/// Only called on downloads
 	func responseBodyReceived(for request: NetworkRequest, bytes: Data)
+	/// Only called on downloads
 	func responseBodyReceived(for request: NetworkRequest, byteCount: Int, totalExpectedToReceive: Int?)
 	func requestFinished(withError error: Error?)
 }
