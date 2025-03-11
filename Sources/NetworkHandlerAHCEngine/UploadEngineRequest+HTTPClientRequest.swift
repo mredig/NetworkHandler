469ad76ff96fd1473096ae90e2d0ad56f1369571
Michael Redig <mredig@gmail.com>
2025-03-11 15:55:01 -0500

    (refactor) remove unused upload async await HTTPClientRequest model

--- Sources/NetworkHandlerAHCEngine/UploadEngineRequest+HTTPClientRequest.swift
+++ Sources/NetworkHandlerAHCEngine/UploadEngineRequest+HTTPClientRequest.swift
@@ -3,15 +3,7 @@ import NIOCore
 import NetworkHandler
 
 extension UploadEngineRequest {
-	public var httpClientRequest: HTTPClientRequest {
-		var request = HTTPClientRequest(url: self.url.absoluteURL.absoluteString)
-		request.method = .init(rawValue: self.method.rawValue)
-		request.headers = .init(self.headers.map { ($0.key.rawValue, $0.value.rawValue) })
-
-		return request
-	}
-
-	var httpClientFutureRequest: HTTPClient.Request {
+	public var httpClientFutureRequest: HTTPClient.Request {
 		get throws {
 			try HTTPClient.Request(
 				url: self.url,
