88e397cd3fd9e1956156caf8c7e82170f93f81e9
Michael Redig <mredig@gmail.com>
2025-03-11 21:23:42 -0500

    (feat) add KnownLengthStream protocol

--- Sources/NetworkHandler/Helpers/InputStreamImprovements.swift
+++ Sources/NetworkHandler/Helpers/InputStreamImprovements.swift
@@ -3,3 +3,7 @@ import Foundation
 public protocol RetryableStream: InputStream {
 	func copyWithRestart() throws(NetworkError) -> Self
 }
+
+public protocol KnownLengthStream: InputStream {
+	var totalStreamBytes: Int? { get }
+}
