d413387e848f4738fd9741507b2dbaac9ff03f2a
Michael Redig <mredig@gmail.com>
2025-03-14 02:04:31 -0500

    (feat) added Sendify

---
+++ Sources/NetworkHandler/Helpers/Sendify.swift
@@ -0,0 +1,29 @@
+import SwiftPizzaSnips
+
+final public class Sendify<T>: @unchecked Sendable {
+	public var value: T {
+		get { lock.withLock { _value } }
+		set { lock.withLock { _value = newValue } }
+	}
+
+	private var _value: T
+
+	private let lock = MutexLock()
+
+	public init(_ value: T) {
+		lock.lock()
+		defer { lock.unlock() }
+		self._value = value
+	}
+}
+
+extension Sendify: Equatable where T: Equatable {
+	public static func == (lhs: Sendify<T>, rhs: Sendify<T>) -> Bool {
+		lhs.value == rhs.value
+	}
+}
+extension Sendify: Hashable where T: Hashable {
+	public func hash(into hasher: inout Hasher) {
+		hasher.combine(value)
+	}
+}
